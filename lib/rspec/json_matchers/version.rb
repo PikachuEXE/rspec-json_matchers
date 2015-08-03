5c02bc448765b203e14669877d6a2e5f592db8ce
PikachuEXE <pikachuexe@gmail.com>
2015-08-03 10:10:14 +0800

    ^ Release 0.1.0.alpha.3

--- lib/rspec/json_matchers/version.rb
+++ lib/rspec/json_matchers/version.rb
@@ -5,6 +5,6 @@ module RSpec
     # The version of the gem
     # Used for generating the value in gemspec file
     # Also can be referenced by code for debugging or other purposes
-    VERSION = "0.1.0.alpha.2"
+    VERSION = "0.1.0.alpha.3"
   end
 end
