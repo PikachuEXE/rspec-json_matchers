bf85edbec9eb8e280573ac5d42e931642a501fdb
PikachuEXE <pikachuexe@gmail.com>
2021-03-25 16:20:53 +0800

    * Support test coverage report on GH Action
    Also switch back from codecov to coveralls

--- Gemfile
+++ Gemfile
@@ -4,5 +4,3 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in rspec-json_matchers.gemspec
 gemspec
-
-gem "codecov", require: false, group: :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
+if ENV["COVERALLS"]
+  require "simplecov"
+  require "simplecov-lcov"
+
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = "coverage/lcov.info"
+  end
+
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+    [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
+  )
+
+  SimpleCov.start do
+    add_filter "spec/"
+  end
+end
+
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 require "rspec-json_matchers"
-
-if ENV["TRAVIS"]
-  require "simplecov"
-  SimpleCov.start
-
-  require "codecov"
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 
 RSpec.configure do |config|
   config.include RSpec::JsonMatchers::Matchers
